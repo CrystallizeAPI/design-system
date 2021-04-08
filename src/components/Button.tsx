89302c2317dd97bb122ec8796eef729ccbc12a93
deekshasharma <deeksha.sharma25@gmail.com>
2021-04-07 18:22:26 -0700

    updated button variations and added stories

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,86 +2,88 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
+  color?: 'primary' | 'secondary' | 'default';
+  variant?: 'filled' | 'outlined' | 'text';
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
+  fullWidth?: boolean;
+  disabled?: boolean;
 }
 
 const StyledButton = styled.button<ButtonProps>`
   font-family: 'Roboto', sans-serif;
-  font-weight: 700;
+  font-weight: 500;
   border: 0;
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
   line-height: 1;
-
+  color: #4A4A4A;
+  background-color: #F6F7F7;
+    ${props =>
+      props.color === 'primary' &&
+      css`
+        background-color: rgb(143, 222, 203);
+      `}
+    ${props =>
+      props.color === 'default' &&
+      css`
+        //box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+      `}
+    ${props =>
+      props.color === 'secondary' &&
+      css`
+        background-color: #fff4e0;
+      `}
+    ${props =>
+      props.size === 'small' &&
+      css`
+        font-size: 12px;
+        padding: 10px 16px;
+      `}
+    ${props =>
+      props.size === 'medium' &&
+      css`
+        font-size: 14px;
+        padding: 11px 20px;
+      `}
+    ${props =>
+      props.size === 'large' &&
+      css`
+        font-size: 16px;
+        padding: 12px 24px;
+      `};
   ${props =>
-    props.primary &&
+    props.fullWidth &&
     css`
-      color: white;
-      background-color: rgb(143, 222, 203);
-    `}
+      width: 100%;
+    `};
   ${props =>
-    !props.primary &&
+    props.disabled &&
     css`
-      color: #333;
-      background-color: transparent;
-      box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
-    `}
-
-  ${props =>
-    props.size === 'small' &&
-    css`
-      font-size: 12px;
-      padding: 10px 16px;
-    `}
-  ${props =>
-    props.size === 'medium' &&
-    css`
-      font-size: 14px;
-      padding: 11px 20px;
-    `}
-  ${props =>
-    props.size === 'large' &&
-    css`
-      font-size: 16px;
-      padding: 12px 24px;
-    `}
+      background-color: #dddddd;
+      color: #9095a8;
+    `};
 `;
 
-/**
- * Primary UI component for user interaction
- */
 export const Button: React.FC<ButtonProps> = ({
-  primary = false,
+  color = 'default',
+  fullWidth = false,
   size = 'medium',
   backgroundColor,
   label,
+  disabled = false,
 }) => {
   return (
     <StyledButton
-      primary={primary}
+      color={color}
       size={size}
       style={{ backgroundColor }}
       label={label}
+      fullWidth={fullWidth}
+      disabled={disabled}
     >
       {label}
     </StyledButton>
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -15,13 +15,20 @@ const Template: Story<ButtonProps> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
+  color: 'primary',
   label: 'Create a document',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   label: 'Create a product',
+  color: 'secondary'
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Add description',
+  color: 'default'
 };
 
 export const Large = Template.bind({});
@@ -35,3 +42,19 @@ Small.args = {
   size: 'small',
   label: '+ Create webhook',
 };
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  size: 'small',
+  label: '+ Create webhook',
+  disabled: true
+};
+
+export const FullWidth = Template.bind({});
+FullWidth.args = {
+  size: 'medium',
+  label: 'ADD TO FAVORITE',
+  fullWidth: true,
+  color: 'primary'
+};
+
