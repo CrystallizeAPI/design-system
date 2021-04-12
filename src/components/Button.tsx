90df1f6a883fefc0ffd4caebce9c13373137ec5a
deekshasharma <deeksha.sharma25@gmail.com>
2021-04-12 16:45:14 -0700

    theme now uses css-variables

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,133 +1,127 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { theme } from '../styles/theme';
+import { GlobalStyle, getRemSize } from '../styles/themev2';
 
 export interface ButtonProps {
-  color?: 'primary' | 'secondary' | 'default';
-  variant?: 'filled' | 'outlined' | 'text';
-  size?: 'small' | 'medium' | 'large';
-  label: string;
+  $color?: 'primary' | 'secondary' | 'default';
+  $variant?: 'filled' | 'outlined' | 'text';
+  $size?: 'small' | 'medium' | 'large';
+  $label: string;
   onClick?: () => void;
-  fullWidth?: boolean;
+  $fullWidth?: boolean;
   disabled?: boolean;
 }
 
 const outlined = css`
-  color: ${theme.palette.text.primary};
+  color: var(--palette-text-primary);
   padding: 5px 15px;
-  border: 0.4px solid ${theme.palette.text.secondary};
-  border-radius: ${theme.borderRadius.default};
+  border: 0.4px solid var(--palette-text-secondary);
+  border-radius: var(--border-radius-default);
   &:hover {
-    background-color: ${theme.palette.action.hover};
+    background-color: var(--palette-action-hover);
   }
   &:disabled {
-    border: 1px solid ${theme.palette.action.disabled};
-    background-color: ${theme.palette.action.disabled};
+    border: 1px solid var(--palette-action-disabled);
+    background-color: var(--palette-action-disabled);
   }
 `;
 
 const outlinedPrimary = css`
-  color: ${theme.palette.primary[600]};
+  color: var(--palette-primary-600);
   background-color: transparent;
-  border: 1px solid ${theme.palette.primary[600]};
+  border: 1px solid var(--palette-primary-600);
   &:hover {
-    border: 1px solid ${theme.palette.primary.dark};
-    background-color: ${theme.palette.primary[400]};
+    border: 1px solid var(--palette-primary-dark);
+    background-color: var(--palette-primary-400);
   }
   &:active {
-    box-shadow: ${theme.shadows[5]};
+    box-shadow: var(--shadow-05);
   }
   &:disabled {
-    border: 1px solid ${theme.palette.action.disabled};
-    color: ${theme.palette.text.disabled};
-    background-color: ${theme.palette.action.disabled};
+    border: 1px solid var(--palette-action-disabled);
+    color: var(--palette-text-disabled);
+    background-color: var(--palette-action-disabled);
   }
 `;
 
 const outlinedSecondary = css`
-  color: ${theme.palette.secondary.dark};
+  color: var(--palette-secondary-dark);
   background-color: transparent;
-  border: 1px solid ${theme.palette.secondary.dark};
+  border: 1px solid var(--palette-secondary-dark);
   &:hover {
-    background-color: ${theme.palette.secondary[400]};
+    background-color: var(--palette-secondary-400);
   }
   &:active {
-    box-shadow: ${theme.shadows[2]};
+    box-shadow: var(--shadow-02);
   }
   &:disabled {
-    border: 1px solid ${theme.palette.action.disabled};
-    color: ${theme.palette.text.disabled};
-    background-color: ${theme.palette.action.disabled};
+    border: 1px solid var(--palette-action-disabled);
+    color: var(--palette-text-disabled);
+    background-color: var(--palette-action-disabled);
   }
 `;
 
 const filled = css`
-  background-color: ${theme.palette.common.white};
-  color: ${theme.palette.primary.contrastText};
-  border-radius: ${theme.borderRadius.default};
-  box-shadow: ${theme.shadows[2]};
+  background-color: var(--palette-common-white);
+  color: var(--palette-primary-contrastText);
+  border-radius: var(--border-radius-default);
+  box-shadow: var(--shadow-02);
   &:hover {
-    background-color: ${theme.palette.action.hover};
-    box-shadow: ${theme.shadows[4]};
+    background-color: var(--palette-action-hover);
+    box-shadow: var(--shadow-04);
   }
   &:active {
-    box-shadow: ${theme.shadows[8]};
+    box-shadow: var(--shadow-08);
   }
   &:focus-visible {
-    box-shadow: ${theme.shadows[6]};
+    box-shadow: var(--shadow-06);
   }
   &:disabled {
-    background-color: ${theme.palette.action.disabled};
-    box-shadow: ${theme.shadows[0]};
+    background-color: var(--palette-action-disabled);
+    box-shadow: var(--shadow-none);
   }
 `;
 
 const filledPrimary = css`
-  background-color: ${theme.palette.primary.main};
-  color: ${theme.palette.primary.contrastText};
-  border-radius: ${theme.borderRadius.default};
+  background-color: var(--palette-primary-main);
+  color: var(--palette-primary-contrastText);
+  border-radius: var(--border-radius-default);
   &:hover {
-    background-color: ${theme.palette.primary.dark};
+    background-color: var(--palette-primary-dark);
   }
   &:disabled {
-    background-color: ${theme.palette.action.disabled};
+    background-color: var(--palette-action-disabled);
   }
 `;
 
 const filledSecondary = css`
-  background-color: ${theme.palette.secondary.main};
-  color: ${theme.palette.secondary.contrastText};
-  border-radius: ${theme.borderRadius.default};
+  background-color: var(--palette-secondary-main);
+  color: var(--palette-secondary-contrastText);
+  border-radius: var(--border-radius-default);
   &:hover {
-    background-color: ${theme.palette.secondary.dark};
+    background-color: var(--palette-secondary-dark);
   }
   &:disabled {
-    background-color: ${theme.palette.action.disabled};
+    background-color: var(--palette-action-disabled);
   }
 `;
 
 const StyledButton = styled.button<ButtonProps>`
-  font-family: ${theme.typography.button.fontFamily};
-  font-weight: ${theme.typography.button.fontWeight};
-  font-size: ${theme.typography.button.fontSize};
   border-style: none;
   outline: 0;
   cursor: pointer;
   display: inline-block;
   padding: 6px 16px;
-  line-height: ${theme.typography.button.lineHeight};;
-  color: ${theme.palette.text.primary};
-  text-transform: ${theme.typography.button.textTransform};
-  letter-spacing: ${theme.typography.button.letterSpacing};
+  color: var(--palette-text-primary);
   background-color: transparent;
     
       /* color=primary and variant=text*/
     ${props =>
-      props.color === 'primary' &&
+      props.$color === 'primary' &&
       css`
-        color: ${theme.palette.primary[600]};
+        color: var(--palette-primary-600);
         &:hover {
-          background-color: ${theme.palette.primary[400]};
+          background-color: var(--palette-primary-400);
         }
         &:disabled {
           background-color: transparent;
@@ -136,11 +130,11 @@ const StyledButton = styled.button<ButtonProps>`
 
       /* color=secondary and variant=text*/
     ${props =>
-      props.color === 'secondary' &&
+      props.$color === 'secondary' &&
       css`
-        color: ${theme.palette.secondary.dark};
+        color: var(--palette-secondary-dark);
         &:hover {
-          background-color: ${theme.palette.secondary[400]};
+          background-color: var(--palette-secondary-400);
         }
         &:disabled {
           background-color: transparent;
@@ -149,73 +143,78 @@ const StyledButton = styled.button<ButtonProps>`
       
       /* size=small*/
     ${props =>
-      props.size === 'small' &&
+      props.$size === 'small' &&
       css`
         padding: 6px 12px;
-        font-size: ${theme.typography.remSize(13)};
+        font-size: ${getRemSize(13)}+rem;
       `}
       
       /* size=large*/
     ${props =>
-      props.size === 'large' &&
+      props.$size === 'large' &&
       css`
         padding: 8px 14px;
-        font-size: ${theme.typography.remSize(15)};
+        font-size: ${getRemSize(15)}+rem;
       `};
 
       /*variant=outlined*/
-    ${props => props.variant === 'outlined' && outlined};
+    ${props => props.$variant === 'outlined' && outlined};
   
     /*variant=filled*/
-    ${props => props.variant === 'filled' && filled};   
+    ${props => props.$variant === 'filled' && filled};   
     ${props =>
-      props.fullWidth &&
+      props.$fullWidth &&
       css`
         width: 100%;
       `};
     ${props =>
       props.disabled &&
       css`
-        color: ${theme.palette.text.disabled};
-        box-shadow: ${theme.shadows[0]};
+        color: var(--palette-text-disabled);
+        box-shadow: var(--shadow-none);
       `};
     ${props =>
-      props.variant === 'outlined' &&
-      props.color === 'primary' &&
+      props.$variant === 'outlined' &&
+      props.$color === 'primary' &&
       outlinedPrimary}; 
     ${props =>
-      props.variant === 'outlined' &&
-      props.color === 'secondary' &&
+      props.$variant === 'outlined' &&
+      props.$color === 'secondary' &&
       outlinedSecondary};
     ${props =>
-      props.variant === 'filled' && props.color === 'primary' && filledPrimary};
+      props.$variant === 'filled' &&
+      props.$color === 'primary' &&
+      filledPrimary};
   ${props =>
-    props.variant === 'filled' &&
-    props.color === 'secondary' &&
+    props.$variant === 'filled' &&
+    props.$color === 'secondary' &&
     filledSecondary};
 
 `;
 
 export const Button: React.FC<ButtonProps> = ({
-  color = 'default',
-  fullWidth = false,
-  size = 'medium',
-  label,
+  $color = 'default',
+  $fullWidth = false,
+  $size = 'medium',
+  $label,
   disabled = false,
-  variant = 'text',
+  $variant = 'text',
   ...props
 }) => {
   return (
-    <StyledButton
-      color={color}
-      size={size}
-      label={label}
-      fullWidth={fullWidth}
-      disabled={disabled}
-      variant={variant}
-      {...props}
-    >
-      {label}
-    </StyledButton>
+    <>
+      <GlobalStyle />
+      <StyledButton
+        $color={$color}
+        $size={$size}
+        $label={$label}
+        $fullWidth={$fullWidth}
+        disabled={disabled}
+        $variant={$variant}
+        {...props}
+      >
+        {$label}
+      </StyledButton>
+    </>
   );
 };
