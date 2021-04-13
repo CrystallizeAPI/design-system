246916a9c5d79ed04cb0c453912bff4986c6cd25
deekshasharma <deeksha.sharma25@gmail.com>
2021-04-13 12:16:15 -0700

    feat: added typography default styles for h1

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import styled, { css } from 'styled-components';
-// import { GlobalStyle } from '../styles/theme';
+import styled from 'styled-components';
+import { GlobalStyle } from '../styles/theme';
 
 //TODO: Should we have a separate style for Button?
 //TODO: Should text color be inherited from the parent?
@@ -25,14 +25,50 @@ export interface TypographyProps {
     | 'body2'
     | 'caption'
     | 'overline';
-  $display?: 'inline' | 'block';
-  $text: string;
+  $display?: 'inline' | 'block' | 'inherit';
+  $text?: string;
   $gutter?: boolean;
-  $textAlign: 'center' | 'left' | 'right' | 'justify';
+  $textAlign?: 'center' | 'left' | 'right' | 'justify' | 'inherit';
   $wrapText?: boolean;
   $textParagraph?: boolean;
 }
 
-export const Typography = () => {
-  return <></>;
+const StyledH1 = styled.h1<TypographyProps>`
+  text-align: inherit;
+  //overflow: hidden;
+  //text-overflow: ellipsis;
+  //white-space: nowrap;
+  margin: 0;
+  display: inherit;
+  text-align: ${props => props.$textAlign};
+  margin-bottom: ${props => (props.$gutter ? `0.35em` : 0)};
+`;
+const StyledH2 = styled.h2<TypographyProps>`
+  color: aquamarine;
+`;
+const StyledH3 = styled.h3<TypographyProps>`
+  color: orange;
+`;
+const StyledH4 = styled.h4<TypographyProps>`
+  color: blueviolet;
+`;
+const StyledH5 = styled.h5<TypographyProps>`
+  color: brown;
+`;
+const StyledH6 = styled.h6<TypographyProps>`
+  color: green;
+`;
+
+export const Typography: React.FC<TypographyProps> = ({ ...props }) => {
+  return (
+    <>
+      <GlobalStyle />
+      {props.$style === 'h1' && <StyledH1 {...props}>{props.$text}</StyledH1>}
+      {props.$style === 'h2' && <StyledH2 {...props}>{props.$text}</StyledH2>}
+      {props.$style === 'h3' && <StyledH3 {...props}>{props.$text}</StyledH3>}
+      {props.$style === 'h4' && <StyledH4 {...props}>{props.$text}</StyledH4>}
+      {props.$style === 'h5' && <StyledH5 {...props}>{props.$text}</StyledH5>}
+      {props.$style === 'h6' && <StyledH6 {...props}>{props.$text}</StyledH6>}
+    </>
+  );
 };
---
+++ stories/Typography.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Typography, TypographyProps } from '../src/components/Typography';
+import { Story, Meta } from '@storybook/react';
+
+export default {
+  title: 'Example/Typography',
+  component: Typography,
+  argTypes: { backgroundColor: { control: 'color' } },
+} as Meta;
+
+const Template: Story<TypographyProps> = args => {
+  return (
+    <div
+      style={{
+        background: 'rgba(0, 0, 0, 0.04)',
+        width: '100%',
+        height: '100%',
+        display: 'inline-block',
+        padding: '10px',
+      }}
+    >
+      <Typography {...args} />
+    </div>
+  );
+};
+
+export const H1 = Template.bind({});
+H1.args = {
+  $text: 'Welcome to Crystallize',
+  $style: 'h1',
+  $textAlign: 'center',
+  $gutter: true,
+};
+
+export const H2 = Template.bind({});
+H2.args = {
+  $text: 'Welcome to Crystallize',
+  $style: 'h2',
+};
+
+export const H3 = Template.bind({});
+H3.args = {
+  $text: 'Welcome to Crystallize',
+  $style: 'h3',
+};
+
+export const H4 = Template.bind({});
+H4.args = {
+  $text: 'Welcome to Crystallize',
+  $style: 'h4',
+};
+
+export const H5 = Template.bind({});
+H5.args = {
+  $text: 'Welcome to Crystallize',
+  $style: 'h5',
+};
+
+export const H6 = Template.bind({});
+H6.args = {
+  $text: 'Welcome to Crystallize',
+  $style: 'h6',
+};
