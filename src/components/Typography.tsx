64ee07593eb199ed361e03d97dc5ca9af256b2ff
deekshasharma <deeksha.sharma25@gmail.com>
2021-04-19 12:31:13 -0700

    refactor: StyledTypographyProps added to prevent TS2339 errors, forwardRef with HTMLSpanElement, removed unused constant

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -30,6 +30,22 @@ export interface TypographyProps {
   children?: React.ReactNode;
 }
 
+/**
+ * This is to prevent error TS2339: Property '$color', $variant etc does not exist on type,
+ * Styled components suggested using $ prefix with a prop when rendering the component. But we don't want consumers to use $ when using this library
+ * https://github.com/styled-components/styled-components/issues/3279.
+ **/
+interface StyledTypographyProps {
+  $color?: TypographyProps['color'];
+  $style?: TypographyProps['style'];
+  $display?: TypographyProps['display'];
+  $gutter?: TypographyProps['gutter'];
+  $align?: TypographyProps['align'];
+  $ellipsis?: TypographyProps['ellipsis'];
+  $textParagraph?: TypographyProps['textParagraph'];
+  $children?: React.ReactNode;
+}
+
 const withEllipsis = css`
   white-space: nowrap;
   overflow: hidden;
@@ -62,22 +78,6 @@ const secondaryTextColor = css`
 
 const errorColor = css`
   color: var(--palette-error-main);
-`;
-
-const TypographyCss = css`
-  font-family: var(--default-font-family);
-  margin: 0;
-  display: ${props => (props.$display ? props.$display : `inherit`)};
-  text-align: ${props => (props.$align ? props.$align : `inherit`)};
-  ${props => props.$ellipsis && props.$display === 'block' && withEllipsis}
-  ${props => props.$textParagraph && paragraph}
-  ${props => props.$gutter && gutterBottom}
-  ${props => props.$color === 'primary' && primaryColor}
-  ${props => props.$color === 'secondary' && secondaryColor}
-  ${props => props.$color === 'secondaryText' && secondaryTextColor}
-  ${props => props.$color === 'error' && errorColor}
-  ${props =>
-    (props.$color === 'primaryText' || !props.$color) && primaryTextColor}
 `;
 
 const Caption = css`
@@ -123,8 +123,20 @@ const Subtitle1 = css`
   letter-spacing: 0.00938em;
 `;
 
-const StyledTypography = styled.p`
-  ${TypographyCss};
+const StyledTypography = styled.span<StyledTypographyProps>`
+  font-family: var(--default-font-family);
+  margin: 0;
+  display: ${props => (props.$display ? props.$display : `inherit`)};
+  text-align: ${props => (props.$align ? props.$align : `inherit`)};
+  ${props => props.$ellipsis && props.$display === 'block' && withEllipsis}
+  ${props => props.$textParagraph && paragraph}
+  ${props => props.$gutter && gutterBottom}
+  ${props => props.$color === 'primary' && primaryColor}
+  ${props => props.$color === 'secondary' && secondaryColor}
+  ${props => props.$color === 'secondaryText' && secondaryTextColor}
+  ${props => props.$color === 'error' && errorColor}
+  ${props =>
+    (props.$color === 'primaryText' || !props.$color) && primaryTextColor}
   ${props => props.$style === 'subtitle1' && Subtitle1}
   ${props => props.$style === 'subtitle2' && Subtitle2}
   ${props => props.$style === 'body1' && Body1}
@@ -133,7 +145,7 @@ const StyledTypography = styled.p`
   ${props => props.$style === 'caption' && Caption}
 `;
 
-export const Typography = React.forwardRef<HTMLHeadingElement, TypographyProps>(
+export const Typography = React.forwardRef<HTMLSpanElement, TypographyProps>(
   ({ ...props }, ref) => {
     return (
       <>
@@ -147,7 +159,7 @@ export const Typography = React.forwardRef<HTMLHeadingElement, TypographyProps>(
           $align={props.align || 'inherit'}
           $ellipsis={props.ellipsis || false}
           $textParagraph={props.textParagraph || false}
-          $as={props.style}
+          as={props.style}
         >
           {props.children}
         </StyledTypography>
