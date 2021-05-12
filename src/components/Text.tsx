0566e585372780849ae6bd9ff67cdf160878af4c
deeksha-crys <deeksha@crystallize.com>
2021-05-12 08:05:23 -0700

    fix: updated the Text API to include size prop. WIP due to TS and Styled components typing issue with polymorhic as

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,6 +4,19 @@ import { GlobalStyle } from '../styles/theme';
 
 export interface TextProps {
   color?: 'primary' | 'secondary' | 'primaryText' | 'secondaryText' | 'error';
+  size?:
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | 'subtitle1'
+    | 'subtitle2'
+    | 'body1'
+    | 'body2'
+    | 'caption'
+    | 'overline';
   style?:
     | 'h1'
     | 'h2'
@@ -35,6 +48,7 @@ export interface TextProps {
 interface StyledTextProps {
   $color?: TextProps['color'];
   $style?: TextProps['style'];
+  $size?: TextProps['size'];
   $underline?: TextProps['underline'];
   $weight?: TextProps['weight'];
   $display?: TextProps['display'];
@@ -118,13 +132,23 @@ const StyledText = styled.span<StyledTextProps>`
   ${props => props.$color === 'error' && errorColor}
   ${props =>
     (props.$color === 'primaryText' || !props.$color) && primaryTextColor}
-  ${props => props.$style === 'subtitle1' && Subtitle1}
-  ${props => props.$style === 'subtitle2' && Subtitle2}
-  ${props => props.$style === 'body1' && Body1}
-  ${props => props.$style === 'body2' && Body2}
-  ${props => props.$style === 'overline' && Overline}
-  ${props => props.$style === 'caption' && Caption}
+  ${props => props.$size === 'subtitle1' && Subtitle1}
+  ${props => props.$size === 'subtitle2' && Subtitle2}
+  ${props => props.$size === 'body1' && Body1}
+  ${props => props.$size === 'body2' && Body2}
+  ${props => props.$size === 'overline' && Overline}
+  ${props => props.$size === 'caption' && Caption}
 `;
+
+const sizeToHeading: Record<string, string> = {
+  0: 'p',
+  1: 'h1',
+  2: 'h2',
+  3: 'h3',
+  4: 'h4',
+  5: 'h5',
+  6: 'h6',
+};
 
 export const Text = React.forwardRef<HTMLSpanElement, TextProps>(
   ({ ...props }, ref) => {
@@ -134,7 +158,8 @@ export const Text = React.forwardRef<HTMLSpanElement, TextProps>(
         <StyledText
           ref={ref}
           $color={props.color || 'primaryText'}
-          $style={props.style || 'subtitle1'}
+          $style={props.style || 'subtitle1'} //TODO: remove
+          $size={props.size || 'subtitle1'}
           $display={props.display || 'inherit'}
           $gutter={props.gutter || false}
           $align={props.align || 'inherit'}
@@ -142,7 +167,7 @@ export const Text = React.forwardRef<HTMLSpanElement, TextProps>(
           $textParagraph={props.textParagraph || false}
           $underline={props.underline || false}
           $weight={props.weight || 'normal'}
-          as={props.style}
+          as={sizeToHeading[props.size || 0] || 'p'}
         >
           {props.children}
         </StyledText>
